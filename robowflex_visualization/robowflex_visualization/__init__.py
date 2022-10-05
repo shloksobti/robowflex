6b8020bec9dcd05a1a7cc0afde2c95bb12871ecf
Shlok Sobti <shlok@canvas.build>
2022-10-05 12:34:18 -0700

    clean up

--- robowflex_visualization/robowflex_visualization/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ["robot", "scene", "primitives", "utils"]
-
-import robowflex_visualization.robot as robot
-import robowflex_visualization.scene as scene
-import robowflex_visualization.primitives as primitives
-import robowflex_visualization.utils as utils
